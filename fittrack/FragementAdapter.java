48582abbc1461677fb0565c78f4189fa5a00e64d
DreamK0324 <87344908+DreamK0324@users.noreply.github.com>
2021-10-28 21:55:55 -0400

    Add files via upload
    Navigation

--- fittrack/FragementAdapter.java
+++ fittrack/FragementAdapter.java
@@ -6,6 +6,9 @@ import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+import com.codepath.fittrack.fragments.LoginFragment;
+import com.codepath.fittrack.fragments.RegisterFragment;
+
 public class FragementAdapter extends FragmentStateAdapter {
     public FragementAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
         super(fragmentManager, lifecycle);
--- fittrack/MainActivity.java
+++ fittrack/MainActivity.java
@@ -1,14 +1,63 @@
 package com.codepath.fittrack;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.codepath.fittrack.fragments.TestFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        fragment = new TestFragment();
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_meal:
+                        fragment = new TestFragment();
+                        Toast.makeText(MainActivity.this, "Meal", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_feed:
+                        fragment = new TestFragment();
+                        Toast.makeText(MainActivity.this, "Feed", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_user:
+                    default:
+                        fragment = new TestFragment();
+                        Toast.makeText(MainActivity.this, "User", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+
+
     }
+
 }
