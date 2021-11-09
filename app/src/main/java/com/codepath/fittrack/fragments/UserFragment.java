4154a53b31d3fea80aab940598e318d2f2b3b924
yiren <yiren987@gmail.com>
2021-11-09 18:52:48 -0500

    parsing data from back4app and displaying

--- app/src/main/java/com/codepath/fittrack/fragments/UserFragment.java
+++ app/src/main/java/com/codepath/fittrack/fragments/UserFragment.java
@@ -7,17 +7,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.codepath.fittrack.LoginActivity;
 import com.codepath.fittrack.R;
+import com.codepath.fittrack.UserInfo;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
-import org.w3c.dom.Text;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,23 +34,18 @@ import org.w3c.dom.Text;
  */
 public class UserFragment extends Fragment {
 
+    public static final String TAG= "UserFragment";
+
     private TextView tvProfileName;
     private TextView tvProfileDescription;
-    private TextView ivProfileImage;
+    private ImageView ivProfileImage;
     private Button btnLogout;
+    private UserInfo currentUserInfo;
 
     public UserFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TestFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static UserFragment newInstance(String param1, String param2) {
         UserFragment fragment = new UserFragment();
@@ -54,7 +57,6 @@ public class UserFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -68,13 +70,12 @@ public class UserFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         tvProfileName = view.findViewById(R.id.tvProfileName);
-
-        //variables that have not been used due to not having a backend holder/variable/info
         tvProfileDescription = view.findViewById(R.id.tvProfileDescription);
         ivProfileImage = view.findViewById(R.id.ivProfileImage);
+        btnLogout = view.findViewById(R.id.btnLogout);
 
-        //sets the profile name to username
-        tvProfileName.setText(ParseUser.getCurrentUser().getUsername());
+        //gets user information
+        queryUser();
 
         //onclick listener to log user out not complete
         btnLogout.setOnClickListener(new View.OnClickListener() {
@@ -86,12 +87,37 @@ public class UserFragment extends Fragment {
 
 
     }
+    private void queryUser() {
+        ParseQuery<UserInfo> query = ParseQuery.getQuery(UserInfo.class);
+        query.include(UserInfo.KEY_USER);
+        query.findInBackground(new FindCallback<UserInfo>() {
+            @Override
+            public void done(List<UserInfo> usersinfo, ParseException e) {
+                if(e!=null){
+                    Log.e(TAG, "Issue with getting users", e);
+                    return;
+                }
+                //Toast.makeText(getActivity(), "hello", Toast.LENGTH_SHORT).show();
+                for(UserInfo user : usersinfo){
+                    if (user.getUser().getUsername().equals(ParseUser.getCurrentUser().getUsername())){
+                        //changing front end
+                        tvProfileName.setText(user.getUser().getUsername());
+                        tvProfileDescription.setText(user.getUserDescription());
+                        ParseFile image = user.getImage();
+                        if(image!=null){
+                            Glide.with(getContext()).load(user.getImage().getUrl()).into(ivProfileImage);
+                        }
+                        return;
+                    }
+                }
+            }
+        });
+    }
 
     private void goLoginActivity(View view) {
-        //TODO:figure out why finish isn't working
         ParseUser.logOut();
         Intent i= new Intent(view.getContext(), LoginActivity.class);
         startActivity(i);
-        //finish();
+        getActivity().finish();
     }
 }
