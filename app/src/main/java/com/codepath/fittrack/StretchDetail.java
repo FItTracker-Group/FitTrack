5831b150b5c2655885c3179059b6a0ca555600f3
DreamK0324 <dreamk0324@gmail.com>
2021-11-08 20:24:48 -0500

    Detail Pages

---
+++ app/src/main/java/com/codepath/fittrack/StretchDetail.java
@@ -0,0 +1,14 @@
+package com.codepath.fittrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StretchDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stretch_detail);
+    }
+}
