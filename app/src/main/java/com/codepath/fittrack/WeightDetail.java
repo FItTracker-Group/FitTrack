5490b8e9a8662638ad02907123c881425b5cb5d7
Kirazuto7 <62526389+Kirazuto7@users.noreply.github.com>
2021-11-10 11:16:00 -0500

    Adding new xmls

---
+++ app/src/main/java/com/codepath/fittrack/WeightDetail.java
@@ -0,0 +1,15 @@
+package com.codepath.fittrack;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class WeightDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight_detail);
+    }
+}
